0f9ddcea5967fb4b8a623c02bb7585b217a376bb
hafidhlaudza6-lgtm <hafidhlaudza6@gmail.com>
2026-09-01 10:47:36 +0700

    praktikum 1 selasa

---
+++ Pekan 1/program1.py
@@ -0,0 +1,13 @@
+# program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + " dibuat oleh a\n"
++ "Hafidh Laudza Mikhrad 2611531019.")
