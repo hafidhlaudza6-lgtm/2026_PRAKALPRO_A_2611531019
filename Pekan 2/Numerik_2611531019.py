6f090f9476f10443fe9a0f7f1541a78a76778c73
hafidhlaudza6-lgtm <hafidhlaudza6@gmail.com>
2026-09-09 16:20:33 +0700

    commit

--- Pekan 2/Numerik_2611531019.py
+++ Pekan 2/Numerik_2611531019.py
@@ -1,11 +1,11 @@
-#create a variabel with integer value
+#create a variable with integer value
 a_1019=100
-print("The type of variabel having value", a_1019, "is", type(a_1019))
+print("The type of variable having value", a_1019, "is", type(a_1019))
 
 #create a variable with float value
 b_1019=10.2345
 print("The type of variable having value", b_1019, "is", type(b_1019))
 
-create a variable with complex value
+#create a variable with complex value
 c_1019=100+3j
 print("The type of variable having value", c_1019, "is", type(c_1019))
